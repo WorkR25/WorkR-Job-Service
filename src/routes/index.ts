28403801d34293f755fe80e74c2b4d75cd2eb735
Arijit Ganguly <aganguly158@gmail.com>
2025-02-23 01:06:54 +0530

    added pm2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,7 @@ import v1Route from './v1';
 async function apiRoute(fastify: FastifyInstance) {
     fastify.register(v1Route, { prefix: '/v1' });
     fastify.get('/check', (_req, res) => {
-        return res.send({ msg: 'alive' });
+        return res.send({ msg: 'alive job server' });
     });
 }
-
 export default apiRoute;
