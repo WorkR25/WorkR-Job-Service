1cec7b32d3534a6cd05aa7d97a4a8d965aecfc61
Arijit Ganguly <aganguly158@gmail.com>
2025-02-24 19:44:31 +0530

    added local host cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const { PORT } = serverConfig;
 const fastify = Fastify();
 
 fastify.register(cors, {
-    origin: ['https://www.workr.club'],
+    origin: ['https://www.workr.club', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'x-access-token'],
     credentials: true
